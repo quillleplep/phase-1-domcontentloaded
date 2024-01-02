4664131c670898e0cd4c68efe4c93c85be8b6cdd
quill_nichols <leplep00100@gmail.com>
2024-01-02 17:43:27 -0500

    neat

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var textElement = document.getElementById('text');
+    textElement.textContent = 'This is really cool!';
+})
 // Your code goes here
